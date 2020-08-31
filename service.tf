a8ce55d61beeccb90a4c7dea60f97853b2cb7660
Toby Clemson <tobyclemson@gmail.com>
2020-08-31 11:25:06 +0100

    Collapse service definitions regardless of absence / presence of load balancer.

--- service.tf
+++ service.tf
@@ -1,10 +1,9 @@
-resource "aws_ecs_service" "service_with_lb" {
-  count = var.attach_to_load_balancer == "yes" ? 1 : 0
+resource "aws_ecs_service" "service" {
   name = var.service_name
   cluster = var.ecs_cluster_id
   task_definition = aws_ecs_task_definition.service.arn
   desired_count = var.service_desired_count
-  iam_role = var.ecs_cluster_service_role_arn
+  iam_role = var.attach_to_load_balancer == "yes" ? var.ecs_cluster_service_role_arn : null
 
   deployment_maximum_percent = var.service_deployment_maximum_percent
   deployment_minimum_healthy_percent = var.service_deployment_minimum_healthy_percent
@@ -19,31 +18,14 @@ resource "aws_ecs_service" "service_with_lb" {
     }
   }
 
-  load_balancer {
-    elb_name = var.service_elb_name
-    target_group_arn = var.target_group_arn
-    container_name = coalesce(var.target_container_name, var.service_name)
-    container_port = coalesce(var.target_port, var.service_port)
-  }
-}
+  dynamic "load_balancer" {
+    for_each = var.attach_to_load_balancer == "yes" ? [var.service_elb_name] : []
 
-resource "aws_ecs_service" "service_without_lb" {
-  count = var.attach_to_load_balancer == "yes" ? 0 : 1
-  name = var.service_name
-  cluster = var.ecs_cluster_id
-  task_definition = aws_ecs_task_definition.service.arn
-  desired_count = var.service_desired_count
-
-  deployment_maximum_percent = var.service_deployment_maximum_percent
-  deployment_minimum_healthy_percent = var.service_deployment_minimum_healthy_percent
-
-  scheduling_strategy = var.scheduling_strategy
-
-  dynamic "placement_constraints" {
-    for_each = var.placement_constraints
     content {
-      type = placement_constraints.value.type
-      expression = placement_constraints.value.expression
+      elb_name = var.service_elb_name
+      target_group_arn = var.target_group_arn
+      container_name = coalesce(var.target_container_name, var.service_name)
+      container_port = coalesce(var.target_port, var.service_port)
     }
   }
 }
