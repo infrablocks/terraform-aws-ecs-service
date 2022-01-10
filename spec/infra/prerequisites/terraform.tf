57e376b82734ede6b8c9b2971fd494273d361c16
Toby Clemson <tobyclemson@gmail.com>
2022-01-10 21:34:50 +0000

    Upgrade all the things.

--- spec/infra/prerequisites/terraform.tf
+++ spec/infra/prerequisites/terraform.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.29"
+      version = "~> 3.71"
     }
     template = {
       source = "hashicorp/template"
-      version = "~> 2.1"
+      version = "~> 2.2"
     }
     null = {
       source = "hashicorp/null"
